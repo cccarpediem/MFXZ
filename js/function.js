5961c944bbc7219ca5d8fdd047f204eba6f3db4d
cccarpediem <ccarpediem@163.com>
2016-05-30 19:23:18 +0800

    version 1.3 by seve7en 5.30

--- js/function.js
+++ js/function.js
@@ -1,3 +1,15 @@
 function changepage(url){
 	window.location.href=url;
 }
+
+function test(){
+	var e = document.getElementById('test');
+	if($('#test').hasClass('navbar_section_show')) {
+		e.className = "navbar_section_hide";
+		e.className += " col-md-8 navbar_section navbar_section_first";
+	}
+	else{
+		e.className = "navbar_section_show";
+		e.className += " col-md-8 navbar_section navbar_section_first";
+	}
+}
