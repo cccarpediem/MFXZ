45d973173f7f352305719ef051aeda54a7a3853d
cccarpediem <ccarpediem@163.com>
2016-06-04 23:33:00 +0800

    version 1.5 by seve7en 6.4 a lot of

--- js/function.js
+++ js/function.js
@@ -7,97 +7,46 @@ window.onload = function() {
     var aCond = aC.getElementsByTagName("div");
 
     var oLi = oDiv.getElementsByTagName("div")[0].getElementsByTagName("a");
-    /*var aCon = aC.getElementsByTagName("div")[0].getElementsByTagName("h3");*/
     var aCDivaa = oCDiv.getElementsByTagName("div")[0].getElementsByTagName("a");
-
-    var timer = null;
-
-   /*for (var i = 0; i < oLi.length; i++) {
-
-        oLi[i].index = i;
-        oLi[i].onclick = function() {
-            show(this.index);
-        }
-    }*/
+    initlist = oDiv.getElementsByTagName("div")[0].getElementsByTagName("a");
+    showcontent(initlist[0]);
 
     for (var i = 0; i < aCDivaa.length; i++){
-    	aCDivaa[i].index = i;
-    	aCDivaa[i].onclick = function() {
-    		show1(this.index);
-    	}	
+        aCDivaa[i].index = i;
+        aCDivaa[i].onclick = function() {
+            show1(this.index);
+        }   
     }
 
     function show1(b){
-    	index = b;
-    	initcontent = (oDiv.getElementsByTagName("div")[b].getElementsByTagName("a"))[0];
-    	showcontent(initcontent);
-    	for(var j = 0; j < aCDivaa.length;j++){
-    		oLid[j].className = "hide";
-    		for(var k = 0; k < aCond.length;k++){
-    			aCond[k].className = "hide";
-    		}
-    	}
-    		oLid[index].className = "";
-    		/*aCond[index].className = "";*/
+        index = b;
+        for(var l = 0; l < aCDivaa.length; l++){
+            aCDivaa[l].className = "";
+            }
+        initcontent = (oDiv.getElementsByTagName("div")[b].getElementsByTagName("a"))[0];
+        initlist = oDiv.getElementsByTagName("div")[b].getElementsByTagName("a");
+        showcontent(initcontent);
+        for(var j = 0; j < aCDivaa.length;j++){
+            oLid[j].className = "hide";
+            for(var k = 0; k < aCond.length;k++){
+                aCond[k].className = "hide";
+            }
+        }  
+            initlist[0].className="show1";
+            aCDivaa[b].className = "show1";
+            oLid[index].className = "";
     }
-
-
-
-
-
-
-
-    /*function show(a) {
-        index = a;
-        var alpha = 0;
-        for (var j = 0; j < oLi.length; j++) {
-            oLi[j].className = "";
-            aCon[j].className = "";
-            aCon[j].style.opacity = 0;
-            aCon[j].style.filter = "alpha(opacity=0)";
-        }
-        oLi[index].className = "cur";
-        aCon[index].className = "cur";
-        clearInterval(timer);
-        timer = setInterval(function() {
-            alpha += 2;
-            alpha > 100 && (alpha = 100);
-            aCon[index].style.opacity = alpha / 100;
-            aCon[index].style.filter = "alpha(opacity=" + alpha + ")";
-            alpha == 100 && clearInterval(timer);
-        },
-        5)
-
-    }*/
 }
-
-
-
-
-
-
-
-function changepage(url){
-	window.location.href=url;
-}
-
-/*function test(){
-	var e = document.getElementById('test');
-	if($('#test').hasClass('navbar_section_show')) {
-		e.className = "navbar_section_hide";
-		e.className += " col-md-8 navbar_section navbar_section_first";
-	}
-	else{
-		e.className = "navbar_section_show";
-		e.className += " col-md-8 navbar_section navbar_section_first";
-	}
-}*/
 
 
 function showcontent(whichcontent){
-	var text = whichcontent.getAttribute("title");
-	var content = document.getElementById("h1");
-	content.firstChild.nodeValue = text;
+    for(var ii = 0; ii < initlist.length; ii++){
+            initlist[ii].className="";
+    }
+    whichcontent.className="show1";
+    var text = whichcontent.getAttribute("src");
+    var content = document.getElementById("h1");
+    content.src = text;
 }
 
 
